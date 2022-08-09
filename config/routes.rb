356e43d5b639e31a73db050bd8330ee3809059dd
ridaarif98 <ridaarif16@gmail.com>
2022-08-09 21:35:45 +0200

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'static/index'
+  root 'static#index'
+  scope '/api' do
+    resources :greetings, only: [:index]
+  end
+  get '*page', to: 'statics#index', constraints: ->(req) do
+  !req.xhr? && req.format.html?
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
