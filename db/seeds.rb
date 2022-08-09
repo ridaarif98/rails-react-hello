96ec9b57290cb8291026d2c0da0f9c3037a95232
ridaarif98 <ridaarif16@gmail.com>
2022-08-09 21:18:21 +0200

    Create a table and add greeting messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Greeting.create!([
+                   {
+                     message: 'Hello World!!'
+                     
+                   },
+                   {
+                     message: 'Bonjour le monde!!'
+                 
+                   },
+                   {
+                     message: 'Jambo Dunia!!'
+                   
+                   },
+                   {
+                     message: 'Muraho neza!!'
+                    
+                   },
+                   {
+                     message: 'Hola Mundo!!'
+                     
+                   }
+                 ])
